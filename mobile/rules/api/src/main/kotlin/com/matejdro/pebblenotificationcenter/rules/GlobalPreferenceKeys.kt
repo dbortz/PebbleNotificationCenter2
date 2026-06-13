620d4a57d81f4f258a770cced07c2e3a157993b2
Dave Bortz <dbortz@gmail.com>
2026-06-13 17:57:57 -0500

    feat(rules): add largeStatusBarFont preference key
    Default off. Backs the new Tools-screen toggle for a larger watch
    status-bar clock font.

--- mobile/rules/api/src/main/kotlin/com/matejdro/pebblenotificationcenter/rules/GlobalPreferenceKeys.kt
+++ mobile/rules/api/src/main/kotlin/com/matejdro/pebblenotificationcenter/rules/GlobalPreferenceKeys.kt
@@ -13,4 +13,6 @@ object GlobalPreferenceKeys {
    val showMessagingStyleChronologically = BooleanPreferenceKeyWithDefault("show_messaging_style_chronologically", false)
 
    val notifyOnReconnect = BooleanPreferenceKeyWithDefault("notify_on_reconnect", true)
+
+   val largeStatusBarFont = BooleanPreferenceKeyWithDefault("large_status_bar_font", false)
 }
